353e0115f2e590831bb98274b875bf023d028ecc
Fatima-zahra Bendnane <fbendnan@c3r8p1.1337.ma>
2026-02-17 23:45:28 +0000

    add

---
+++ generate_maze.py
@@ -0,0 +1,65 @@
+import random
+
+
+class cell_info:
+    def __init__(self, cell):
+        self.cell = cell
+        self.is_visited = False
+        self.walls = {}
+
+    def fill_cell_walls(self):
+        self.walls['N'] = (((self.cell[0]), (self.cell[1] - 1)), (self.cell))
+        self.walls['W'] = (((self.cell[0] - 1), (self.cell[1])), (self.cell))
+        self.walls['E'] = (((self.cell[0] + 1), (self.cell[1])), self.cell)
+        self.walls['S'] = (((self.cell[0]), (self.cell[1] + 1)), self.cell)
+
+
+class maze_manager:
+    def __init__(self):
+        self.frontier_edges = set()
+        self.grid = {}
+        self.walls_to_remove = set()
+
+    def initialise_grid(self, height, width):
+        for x in range(height):
+            for y in range(width):
+                c = cell_info((x, y))
+                c.fill_cell_walls()
+                self.grid[(x, y)] = c
+
+    def add_edge_from_cell(self, cell):
+        for wall in cell.walls.values():
+            neighbor_coord, current_coord = wall
+            if neighbor_coord in self.grid:
+                self.frontier_edges.add(wall)
+
+    def mark_start_cell(self, start_x, start_y):
+        start = self.grid[(start_x, start_y)]
+        start.is_visited = True
+        self.add_edge_from_cell(start)
+
+    def remove_edge(self, edge):
+        self.frontier_edges.remove(edge)
+
+
+def generate_maze_with_prim_algo():
+    start_point = (12, 15)
+    maze = maze_manager()
+    maze.initialise_grid(start_point[0], start_point[1])
+    maze.mark_start_cell(2, 3)
+    while maze.frontier_edges:
+        edge = random.choice(list(maze.frontier_edges))
+        maze.remove_edge(edge)
+
+        neighbor_coord, current_coord = edge
+        cell_a = maze.grid[neighbor_coord]
+        cell_b = maze.grid[current_coord]
+
+        if cell_a.is_visited != cell_b.is_visited:
+            new_cell = cell_b if cell_a.is_visited else cell_a
+            new_cell.is_visited = True
+            maze.walls_to_remove.add(edge)
+            maze.add_edge_from_cell(new_cell)
+
+
+generate_maze_with_prim_algo()
---
+++ parssing.py
@@ -0,0 +1,95 @@
+def mandatory_exist(configuration: dict):
+    mandatory = ['WIDTH', 'HEIGHT', 'ENTRY', 'EXIT', 'OUTPUT_FILE', 'PERFECT']
+    for m in mandatory:
+        if m not in configuration:
+            return False
+    return True
+
+
+def main():
+    configuration = {}
+    try:
+        with open('config.txt', 'r') as configuratin_file:
+            for line in configuratin_file:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                if '=' not in line:
+                    raise ValueError(
+                        "Invalid line format,"
+                        " the line should containe key=value"
+                    )
+                line_s = line.split('=')
+                to_check = line_s[0].strip().upper()
+                if to_check == 'WIDTH' or to_check == 'HEIGHT' \
+                   or to_check == 'SEED':
+                    if to_check in configuration:
+                        raise ValueError(
+                            "you shouldn't duplicate a parametter"
+                            )
+                    else:
+                        value = line_s[1].strip('\n')
+                        configuration[to_check] = int(value)
+                elif to_check == 'ENTRY' or to_check == 'EXIT':
+                    if to_check in configuration:
+                        raise ValueError(
+                            "you shouldn't duplicate a parametter"
+                            )
+                    coord = line_s[1].split(',')
+                    if len(coord) == 2:
+                        configuration[to_check] = (
+                            int(coord[0]), int(coord[1])
+                            )
+                    else:
+                        raise ValueError(
+                            "in Entry and Exit you must enter 2 values"
+                            )
+                elif to_check == 'PERFECT':
+                    if to_check in configuration:
+                        raise ValueError(
+                            "you shouldn't duplicate a parametter"
+                            )
+                    if line_s[1].strip().upper() == 'TRUE':
+                        configuration[to_check] = True
+                    elif line_s[1].strip().upper() == 'FALSE':
+                        configuration[to_check] = False
+                elif to_check == 'OUTPUT_FILE':
+                    if to_check in configuration:
+                        raise ValueError(
+                            "you shouldn't duplicate a parametter"
+                            )
+                    configuration[to_check] = line_s[1].strip('\n')
+                else:
+                    raise ValueError(
+                        "you enter a parameter does't needed...!"
+                        )
+            if len(configuration) < 6 \
+                    or mandatory_exist(configuration) is False:
+                raise ValueError(
+                    "you should enter all the mandatory data needed: "
+                    "'WIDTH', 'HEIGHT', 'ENTRY', 'EXIT', "
+                    "'OUTPUT_FILE', 'PERFECT'"
+                    )
+
+            entry = configuration['ENTRY']
+            exit = configuration['EXIT']
+            width = configuration['WIDTH']
+            height = configuration['HEIGHT']
+            proba1: bool = (entry[0] < width and entry[1] == 0) \
+                and (exit[0] < width and exit[1] == height - 1)
+            proba2: bool = (entry[0] < width and entry[1] == height - 1) \
+                and (exit[0] < width and exit[1] == 0)
+            proba3: bool = (entry[0] == 0 and entry[1] < height) \
+                and (exit[0] == width - 1 and exit[1] < height)
+            proba4: bool = (entry[0] == width - 1 and entry[1] < height) \
+                and (exit[0] == 0 and exit[1] < height)
+            if proba1 is False and proba2 is False and proba3 is False \
+               and proba4 is False:
+                raise ValueError("The entry and Exit are invalid "
+                                 "for this maze")
+        print(configuration)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+main()
