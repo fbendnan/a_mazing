ffb5b637387f6480ab4bd517c690b69cfc771767
Fatima-zahra Bendnane <fbendnan@c3r9p5.1337.ma>
2026-02-20 17:41:18 +0000

    add

--- draw_maze.py
+++ draw_maze.py
@@ -1,15 +1,38 @@
 from mlx.mlx import Mlx
-import sys
 
-mlx = Mlx()
 
-mlx_ptr = mlx.mlx_init()
-win_ptr = mlx.mlx_new_window(mlx_ptr, 800, 600, "Maze window")
+CELL_SIZE = 40
 
-mlx.mlx_pixel_put(mlx_ptr, win_ptr, 200, 200, 0xFFFFFF)
+RED_COLOR = 0x00FF0000
 
-try:
-    mlx.mlx_loop(mlx_ptr)
-except KeyboardInterrupt:
-    print("Program stopped")
-    sys.exit(0)
+class Display_maze:
+    def __init__(self, height, width, grid):
+        self.height = height
+        self.width = width
+        self.grid = grid
+        self.mlx
+        self.mlx_ptr
+        self.win_ptr
+
+
+    def initialization(self):
+        self.mlx = Mlx()
+        self.mlx_ptr = self.mlx.mlx_init()
+
+    def windows(self):
+        self.win_ptr = self.mlx.mlx_new_window(
+            self.mlx_ptr, self.width, self.height, "maze"
+            )
+    
+    def draw_walls(self, col, row):
+        x = col * CELL_SIZE
+        y = row * CELL_SIZE
+        for cell in self.grid:
+            if cell.walls['N']:
+                self.mlx.mlx_pixel_put(self.mlx_ptr, self.win_ptr, x, y, RED_COLOR)
+            if cell.walls['S']:
+                self.mlx.mlx_pixel_put(self.mlx_ptr, self.win_ptr, x, y, RED_COLOR)
+            if cell.walls['E']:
+                self.mlx.mlx_pixel_put(self.mlx_ptr, self.win_ptr, x, y, RED_COLOR)
+            if cell.walls['W']:
+                self.mlx.mlx_pixel_put(self.mlx_ptr, self.win_ptr, x, y, RED_COLOR)
