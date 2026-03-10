756332a44fa7ecf5cd4ba603d6e80b0406979877
Fatima-zahra Bendnane <fbendnan@c3r9p3.1337.ma>
2026-03-10 00:06:18 +0000

    add

--- mazegen/Cell.py
+++ mazegen/Cell.py
@@ -1,10 +1,20 @@
 from typing import List, Dict
 
+
 class Cell:
-    def __init__(self, x, y) -> None:
+    def __init__(self, x: int, y: int) -> None:
         self.x: int = x
         self.y: int = y
+
         self.is_visited: bool = False
-        self.walls: Dict = {'N': True, 'E': True, 'S': True, 'W': True}
+
+        self.walls: Dict[str, bool] = {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True
+        }
+
         self.is_cell_42: bool = False
-        self.solver_walls: List = []
+
+        self.solver_walls: List[int] = []
